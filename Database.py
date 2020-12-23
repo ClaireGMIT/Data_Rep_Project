e85a26528b7235525ede5bb6a4bc22a20c4f75f5
Claire Laptop <clairenolan@hotmail.com>
2020-12-23 15:45:47 +0000

    22DEC2020 work

--- Database.py
+++ Database.py
@@ -12,7 +12,7 @@ class ProductionDAO:
 
     def create(self, values):
         cursor = self.db.cursor()
-        sql="insert into production (Batch_No, API_Particle_Size, Screen_Size, Blend_Time, Compressor, Inlet_Temp, Spray_Rate, Dissolution) values (%s,%s, %s,%s, %s,%s, %s,%s)"
+        sql="insert into production (Batch_No, API_Lot_No, API_Particle_Size, Screen_Size, Blend_Time, Compressor, Inlet_Temp, Spray_Rate, Dissolution) values (%s,%s, %s,%s, %s,%s, %s,%s)"
         cursor.execute(sql, values)
         self.db.commit()
         return cursor.lastrowid
@@ -34,7 +34,7 @@ class ProductionDAO:
     
     def update(self, values):
         cursor = self.db.cursor()
-        sql= "update production set API_Particle_Size=%s, Screen_Size=%s, Blend_Time=%s, Compressor=%s, Inlet_Temp=%s, Spray_Rate=%s, Dissolution%s where Batch_No = %s"
+        sql= "update production set API_Lot_No%s, API_Particle_Size=%s, Screen_Size=%s, Blend_Time=%s, Compressor=%s, Inlet_Temp=%s, Spray_Rate=%s, Dissolution%s where Batch_No = %s"
         cursor.execute(sql, values)
         self.db.commit()
 
---
+++ initdb.sql
@@ -0,0 +1,15 @@
+
+
+
+CREATE TABLE TabPrdn (
+    -> Batch_No varchar(10) NOT NULL,
+    -> API_Lot_No varchar(10) NOT NULL,
+    -> API_Particle_Size enum('Large', 'Medium', 'Small') NOT NULL,
+    -> Screen_Size enum('3', '4', '5') NOT NULL,
+    -> Blend_Time float(3.1) NOT NULL,
+    -> Compressor enum('Compress1', 'Compress2') NOT NULL,
+    -> Inlet_Temp float(4.1) NOT NULL,
+    -> Spray_Rate float(4.1) NOT NULL,
+    -> Dissolution float(3.2) NOT NULL,
+    -> PRIMARY KEY (Batch_No)
+    -> );
